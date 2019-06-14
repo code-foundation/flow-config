f22cf95e0fcdb06bb955906d4154a9f228e1181f
Edward Murrell <edward.murrell@codefoundation.com.au>
2019-06-14 17:28:21 +1000

    Red test for exceptions when modifying the immutable keys of config item.

---
+++ src/Exceptions/EntityKeyChangeException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace CodeFoundation\FlowConfig\Exceptions;
+
+/**
+ * This exception is thrown when the keys of an existing entity are modified.
+ */
+class EntityKeyChangeException extends \Exception
+{
+}
--- tests/Entities/EntityConfigItemTest.php
+++ tests/Entities/EntityConfigItemTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace CodeFoundation\FlowConfig\Tests\Entity;
 
 use CodeFoundation\FlowConfig\Entity\EntityConfigItem;
+use CodeFoundation\FlowConfig\Exceptions\EntityKeyChangeException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -23,5 +24,44 @@ class EntityConfigItemTest extends TestCase
         self::assertSame('123456', $configItem->getEntityId());
         self::assertSame('user', $configItem->getEntityType());
         self::assertSame('value', $configItem->getValue());
+    }
+
+    /**
+     * Test exceptions are thrown when modifying the key of a config item.
+     */
+    public function testExceptionWhenChangingKey(): void
+    {
+        $configItem = new EntityConfigItem();
+        $configItem->setKey('key');
+
+        $this->expectException(EntityKeyChangeException::class);
+
+        $configItem->setKey('newkey');
+    }
+
+    /**
+     * Test exceptions are thrown when modifying the EntityId of a config item.
+     */
+    public function testExceptionWhenChangingEntityId(): void
+    {
+        $configItem = new EntityConfigItem();
+        $configItem->setEntityId('abc123');
+
+        $this->expectException(EntityKeyChangeException::class);
+
+        $configItem->setEntityId('xyz000');
+    }
+
+    /**
+     * Test exceptions are thrown when modifying the EntityId of a config item.
+     */
+    public function testExceptionWhenChangingEntityType(): void
+    {
+        $configItem = new EntityConfigItem();
+        $configItem->setEntityType('user');
+
+        $this->expectException(EntityKeyChangeException::class);
+
+        $configItem->setEntityType('customer');
     }
 }
