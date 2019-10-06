3d82261b590bda86ae4ebfbf58b8caa7a19680b2
Brendon Tyree <brendon@tyree.co.nz>
2019-10-07 08:51:31 +1100

    Updated .gitignore, removed constants from interface

--- src/Interfaces/Accessibility/AccessibilityInterface.php
+++ src/Interfaces/Accessibility/AccessibilityInterface.php
@@ -6,21 +6,6 @@ namespace CodeFoundation\FlowConfig\Interfaces\Accessibility;
 interface AccessibilityInterface
 {
     /**
-     * They key is hidden from bulk gets by default, but can be specifically requested.
-     */
-    public const ACCESSIBILITY_HIDDEN = 0;
-
-    /**
-     * The key is used internally and never returned in bulk gets.
-     */
-    public const ACCESSIBILITY_INTERNAL = 1;
-
-    /**
-     * They key is read-only, and cannot be changed.
-     */
-    public const ACCESSIBILITY_READONLY = 2;
-
-    /**
      * Gets whether the specified key can be retrieved.
      *
      * @param string $key
